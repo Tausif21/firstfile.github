80921c5d9faaa33e0cb2cc4b00b3ab57da226f79
Md Tausif Raza <107751376+Tausif21@users.noreply.github.com>
2022-06-26 22:57:00 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number:";
+    cin >> num;
+    cout << "Hello World \n";
+    cout << num;
+    return 0;
+}
